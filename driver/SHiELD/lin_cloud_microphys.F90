19b48bc1f3c6a86ff7eb73fc9c219e8020220c58
Rusty.Benson <rusty.benson@noaa.gov>
2019-07-19 13:31:38 -0400

    fixed license header in every file and added it to those without it

--- driver/SHiELD/lin_cloud_microphys.F90
+++ driver/SHiELD/lin_cloud_microphys.F90
@@ -1,3 +1,23 @@
+!***********************************************************************
+!*                   GNU Lesser General Public License
+!*
+!* This file is part of the FV3 dynamical core.
+!*
+!* The FV3 dynamical core is free software: you can redistribute it
+!* and/or modify it under the terms of the
+!* GNU Lesser General Public License as published by the
+!* Free Software Foundation, either version 3 of the License, or
+!* (at your option) any later version.
+!*
+!* The FV3 dynamical core is distributed in the hope that it will be
+!* useful, but WITHOUT ANYWARRANTY; without even the implied warranty
+!* of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+!* See the GNU General Public License for more details.
+!*
+!* You should have received a copy of the GNU Lesser General Public
+!* License along with the FV3 dynamical core.
+!* If not, see <http://www.gnu.org/licenses/>.
+!***********************************************************************
 !
 ! Cloud micro-physics package for GFDL global cloud resolving model
 ! The algorithms are originally derived from Lin et al 1983. Most of the key
