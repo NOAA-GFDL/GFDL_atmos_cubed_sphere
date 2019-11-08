b400a5c1b20abf0ffe83eea3460ec120f5ac3a11
Philip Pegion <Philip.Pegion@noaa.gov>
2019-11-08 17:58:47 +0000

    fix of openmp directives for dissipation estimate

--- model/dyn_core.F90
+++ model/dyn_core.F90
@@ -364,7 +364,7 @@ contains
                allocate( dv(isd:ied+1,jsd:jed,  npz) )
                call init_ijk_mem(isd,ied+1, jsd,jed  , npz, dv, 0.)
           endif
-!$OMP parallel do default(none) shared(is,ie,js,je,npz,diss_est)
+!$OMP parallel do default(none) shared(isd,ied,jsd,jed,npz,diss_est)
           do k=1,npz
             do j=jsd,jed
               do i=isd,ied
