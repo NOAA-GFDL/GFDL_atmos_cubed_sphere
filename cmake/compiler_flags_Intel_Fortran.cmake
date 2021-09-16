34806e77e9d10964a5abd48c6c7e0a36e752ff6d
Rahul Mahajan <aerorahul@users.noreply.github.com>
2021-09-16 14:05:56 -0400

    update Intel flags to be consistent w/ UFS

--- cmake/compiler_flags_Intel_Fortran.cmake
+++ cmake/compiler_flags_Intel_Fortran.cmake
@@ -4,7 +4,7 @@ set(r8_flags "-real-size 64") # Fortran flags for 64BIT precision
 set(r8_flags "${r8_flags} -no-prec-div -no-prec-sqrt")
 
 # Intel Fortran
-set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -g -traceback -fpp -fno-alias -auto -safe-cray-ptr -ftz -assume byterecl -nowarn -sox -align array64byte -qno-opt-dynamic-align")
+set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -g -traceback -fpp -fno-alias -auto -safe-cray-ptr -ftz -assume byterecl -nowarn -sox -align array64byte -qno-opt-dynamic-align ${${fms_kind}_flags}")
 
 set(CMAKE_Fortran_FLAGS_REPRO "-O2 -debug minimal -fp-model consistent -qoverride-limits")
 
@@ -14,4 +14,12 @@ set(CMAKE_Fortran_FLAGS_DEBUG "-O0 -check -check noarg_temp_created -check nopoi
 
 set(CMAKE_Fortran_LINK_FLAGS "")
 
-set(FAST "-fast-transcendentals")
+if(NOT CMAKE_BUILD_TYPE MATCHES "^(Debug|Repro)$")
+  set(FAST "-fast-transcendentals")
+  if(AVX2)
+    set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -march=core-avx2")
+  elseif(SIMDMULTIARCH)
+    set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -axSSE4.2,CORE-AVX2")
+  endif()
+endif()
+
