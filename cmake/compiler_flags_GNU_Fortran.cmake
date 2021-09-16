ba1ea863729796f651237db062d67f42156ec576
Rahul Mahajan <aerorahul@users.noreply.github.com>
2021-09-16 15:25:21 -0400

    update GNU flags

--- cmake/compiler_flags_GNU_Fortran.cmake
+++ cmake/compiler_flags_GNU_Fortran.cmake
@@ -2,14 +2,14 @@
 set(r8_flags "-fdefault-real-8 -fdefault-double-8") # Fortran flags for 64BIT precision
 
 # GNU Fortan
-set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -ggdb -fbacktrace -cpp -fcray-pointer -ffree-line-length-none -fno-range-check")
+set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -ggdb -fbacktrace -cpp -fcray-pointer -ffree-line-length-none -fno-range-check ${${fms_kind}_flags}")
 if(${CMAKE_Fortran_COMPILER_VERSION} VERSION_GREATER_EQUAL 10)
     set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -fallow-argument-mismatch -fallow-invalid-boz")
 endif()
 
-set(CMAKE_Fortran_FLAGS_REPRO "-O2 -funroll-all-loops -finline-functions")
+set(CMAKE_Fortran_FLAGS_REPRO "-O2")
 
-set(CMAKE_Fortran_FLAGS_RELEASE "-O3 -funroll-all-loops -finline-functions")
+set(CMAKE_Fortran_FLAGS_RELEASE "-O2")
 
 set(CMAKE_Fortran_FLAGS_DEBUG "-O0 -fno-unsafe-math-optimizations -frounding-math -fsignaling-nans -ffpe-trap=invalid,zero,overflow -fbounds-check")
 
