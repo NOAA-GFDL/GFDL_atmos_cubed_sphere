5c0d250a46a919c1333bb897e404e93227a0f9a2
Rahul Mahajan <aerorahul@users.noreply.github.com>
2021-09-16 17:00:43 -0400

    move kind out of FMS found block

--- cmake/compiler_flags_GNU_Fortran.cmake
+++ cmake/compiler_flags_GNU_Fortran.cmake
@@ -2,7 +2,7 @@
 set(r8_flags "-fdefault-real-8 -fdefault-double-8") # Fortran flags for 64BIT precision
 
 # GNU Fortan
-set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -ggdb -fbacktrace -cpp -fcray-pointer -ffree-line-length-none -fno-range-check ${${fms_kind}_flags}")
+set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -ggdb -fbacktrace -cpp -fcray-pointer -ffree-line-length-none -fno-range-check ${${kind}_flags}")
 if(${CMAKE_Fortran_COMPILER_VERSION} VERSION_GREATER_EQUAL 10)
     set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -fallow-argument-mismatch -fallow-invalid-boz")
 endif()
--- cmake/compiler_flags_Intel_Fortran.cmake
+++ cmake/compiler_flags_Intel_Fortran.cmake
@@ -4,7 +4,7 @@ set(r8_flags "-real-size 64") # Fortran flags for 64BIT precision
 set(r8_flags "${r8_flags} -no-prec-div -no-prec-sqrt")
 
 # Intel Fortran
-set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -g -traceback -fpp -fno-alias -auto -safe-cray-ptr -ftz -assume byterecl -nowarn -sox -align array64byte -qno-opt-dynamic-align ${${fms_kind}_flags}")
+set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -g -traceback -fpp -fno-alias -auto -safe-cray-ptr -ftz -assume byterecl -nowarn -sox -align array64byte -qno-opt-dynamic-align ${${kind}_flags}")
 
 set(CMAKE_Fortran_FLAGS_REPRO "-O2 -debug minimal -fp-model consistent -qoverride-limits")
 
