2dc7e47009c5e2c9c457ae21320324510a59a87a
larissa.reames@noaa.gov <larissa.reames@noaa.gov>
2022-02-16 22:01:00 +0000

    Update to idealized test cases to allow for intialization from an external sounding
    t_profile,q_profile,ws_profile = -1 to use this option with "input_sounding" in run directory

--- tools/test_cases.F90
+++ tools/test_cases.F90
@@ -175,16 +175,18 @@
 !                  101 = 3D non-hydrostatic Large-Eddy-Simulation (LES) with hybrid_z IC
 
       integer :: sphum, theta_d
+      integer, parameter :: max_bub=100
       real(kind=R_GRID), parameter :: radius = cnst_radius
       real(kind=R_GRID), parameter :: one = 1.d0
       integer :: test_case = 11
       logical :: bubble_do = .false.
       real    :: alpha = 0.0
-      integer :: Nsolitons = 1
+      integer :: Nsolitons = 1, n_bub = 1
       real    :: soliton_size = 750.e3, soliton_Umax = 50.
       integer :: t_profile = 0, q_profile = 0, ws_profile = 0, do_coriolis = 0, bubble_type = 0
       real    :: bubble_t = 2., bubble_q = 0., bubble_rad = 10.0E3, bubble_zc = 1.4E3
       real    :: iso_t = 300., adi_th = 300., us0 = 30.
+      real,dimension(max_bub)    :: icenters, jcenters
 
 ! Case 0 parameters
       real :: p0_c0 = 3.0
@@ -4655,6 +4657,8 @@ end subroutine terminator_tracers
         type(fv_grid_type), target :: gridstruct
         type(fv_flags_type), target :: flagstruct
 
+        integer, parameter :: nl_max = 2500
+        real, dimension(nl_max) ::  z_snd, p_snd, t_snd, rho_snd, u_snd, v_snd, qv_snd
         real, dimension(bd%is:bd%ie):: pm, qs
         real, dimension(1:npz):: pk1, ts1, qs1, dummy
         !real :: us0 = 30.
@@ -4663,8 +4667,9 @@ end subroutine terminator_tracers
         real :: t00, p00, xmax, xc, xx, yy, pk0, pturb, ztop
         real :: ze1(npz+1)
          real:: dz1(npz)
-        real:: zvir
-        integer :: i, j, k, m, icenter, jcenter
+        real:: zvir, rand1, rand2
+        real:: amplitude = 0.2
+        integer :: i, j, k, m, icenter, jcenter, nl_snd
 
         real, pointer, dimension(:,:,:)   :: agrid, grid
         real(kind=R_GRID), pointer, dimension(:,:)     :: area
@@ -4688,7 +4693,7 @@ end subroutine terminator_tracers
         integer :: is,  ie,  js,  je
         integer :: isd, ied, jsd, jed
    
-        integer :: n_bub, b
+        integer :: b
 
         is  = bd%is
         ie  = bd%ie
@@ -5331,7 +5336,14 @@ end subroutine terminator_tracers
            pk1(k) = (pk(i,j,k+1)-pk(i,j,k))/(kappa*(peln(i,k+1,j)-peln(i,k,j)))
         enddo
 
-        if ( t_profile == 0 ) then
+        if (t_profile == -1 .or. q_profile == -1 .or. ws_profile == -1) then
+          call get_sounding( z_snd, p_snd, t_snd, rho_snd, u_snd, v_snd, qv_snd, nl_max, nl_snd )
+        endif
+        if (t_profile == -1) then
+          do k = 1,npz
+           ts1(k) =  interp_log( t_snd, p_snd, pe(i,k,j), nl_max, nl_snd  )
+          enddo
+        elseif ( t_profile == 0 ) then
           call SuperCell_Sounding(npz, p00, pk1, ts1, qs1)
         elseif (t_profile == 1 ) then
           !adiabatic
@@ -5347,7 +5359,11 @@ end subroutine terminator_tracers
           call mpp_error(FATAL, " t_profile ", t_profile ,"  not defined" )
         endif
 
-        if ( q_profile==0 ) then
+        if (q_profile == -1) then
+          do k = 1,npz
+           qs1(k) =  interp_log( qv_snd, p_snd, pe(i,k,j), nl_max,nl_snd  )
+          enddo
+        elseif ( q_profile==0 ) then
           if (t_profile == 0) then
             ! qs1 already computed prior, move along
           else
@@ -5379,7 +5395,24 @@ end subroutine terminator_tracers
            ze1(k) = ze1(k+1) - delz(is,js,k)
         enddo
 
-        if ( ws_profile==0 ) then
+        if (ws_profile == -1) then
+          do k = 1,npz
+           utmp =  interp_lin( u_snd, z_snd, ze1(k), nl_max, nl_snd  )
+           vtmp = interp_lin( v_snd, z_snd,ze1(k), nl_max, nl_snd  )
+           if(is_master()) print*, "k, z, u, v = ",k, ze1(k),utmp, vtmp  
+            do j=js,je+1
+              do i=is,ie
+                 u(i,j,k) = utmp
+             enddo
+           enddo
+            do j=js,je
+              do i=is,ie+1
+                v(i,j,k) = vtmp
+              enddo
+            enddo
+          enddo
+             
+        elseif ( ws_profile==0 ) then
         ! Quarter-circle hodograph (Harris approximation)
           do k=1,npz
            zm = 0.5*(ze1(k)+ze1(k+1))
@@ -5428,6 +5461,17 @@ end subroutine terminator_tracers
         elseif (ws_profile==4 ) then
           u(:,:,:) = 0.
           v(:,:,:) = 0.
+        elseif (ws_profile==5) then
+          ! Linear WK shear below 2.5km
+          v(:,:,:) = 0.
+          do k=1,npz
+            zm = 0.5*(ze1(k)+ze1(k+1))
+            if ( zm .le. 2.5e3 ) then
+              u(:,:,k) = us0 * tanh(zm/3.e3)
+            else
+              u(:,:,k) = us0
+            endif
+          enddo
         else
           call mpp_error(FATAL, " ws_profile ", ws_profile ,"  not defined" )
         endif
@@ -5446,24 +5490,34 @@ end subroutine terminator_tracers
             jcenter = (npy-1)/2 + 1
             n_bub = 1
           elseif (bubble_type == 2) then
-            n_bub = floor(float(npy)*dy_const/r0)
-            print*, "initializing ", n_bub , " bubbles"
-            icenter = 3 * ceiling(r0/dy_const)
-            jcenter = 0
+            !Line of centered N-S bubbles for squall line 
+            !n_bub = floor(float(npy)*dy_const/r0)
+            if ( is_master() )  print*, "initializing ", n_bub , " bubbles"
+            icenter = 0
+          elseif (bubble_type == 3) then
+            ! User entry of i/j bubble locations
+            if ( is_master() ) print*, "initializing ", n_bub , " bubbles"
+            if ( is_master() ) print*, "at locations i = ", icenters(1:n_bub), "j = ", jcenters(1:n_bub)
           endif
           do b = 1, n_bub
-            if (bubble_type == 2) jcenter = jcenter + ceiling(r0/dy_const)
-            print*, "icenter, jcenter =", icenter, jcenter
+            if (bubble_type == 2) jcenter=((npy-1)/2+1)-((n_bub+1)/2-b)*30000.0/dy_const
+            if (bubble_type == 3) then
+              icenter = icenters(b)
+              jcenter = jcenters(b)
+            endif
+            if ( is_master() ) print*, "icenter, jcenter =", icenter, jcenter
             do k=1, npz
               zm = 0.5*(ze1(k)+ze1(k+1))
               ptmp = ( (zm-zc)/zc ) **2
               if ( ptmp < 1. ) then
                 do j=js,je
                   do i=is,ie
-                    dist =ptmp+((i-icenter)*dx_const/r0)**2+((j-jcenter)*dy_const/r0)**2
+                    call random_number(rand1)
+                    call random_number(rand2)
+                    dist=ptmp+((i-icenter)*dx_const/r0)**2+((j-jcenter)*dy_const/r0)**2
                     if ( dist < 1. ) then
-                      pt(i,j,k) = pt(i,j,k) + pturb*(1.-sqrt(dist))
-                      q(i,j,k,1)= q(i,j,k,1) + bubble_q*(1.-sqrt(dist))
+                      pt(i,j,k) = pt(i,j,k) + pturb*(1.-sqrt(dist)) +amplitude*(2.0*rand1-1.0)
+                      q(i,j,k,1)= q(i,j,k,1) + bubble_q*(1.-sqrt(dist)) +amplitude*1.E-6*(2.0*rand2-1.0)
                     endif !dist
                   enddo !i
                 enddo !j
@@ -5527,7 +5581,7 @@ end subroutine terminator_tracers
         integer :: ierr, f_unit, unit, ios
         namelist /test_case_nml/test_case, bubble_do, alpha, nsolitons, soliton_Umax, soliton_size, &
                                 t_profile, q_profile, ws_profile, bubble_t, bubble_q, bubble_rad, &
-                                bubble_zc, do_coriolis, iso_t, adi_th, us0, bubble_type
+                                bubble_zc, do_coriolis, iso_t, adi_th, us0, bubble_type,n_bub,icenters,jcenters
 
 #include<file_version.h>
 
@@ -7968,4 +8022,210 @@ subroutine qs_table (n)
 
 end subroutine qs_table
 
+    subroutine get_sounding( zk, p, t, rho, u, v, qv, nl_max, nl_in )
+      implicit none
+
+      integer nl_max, nl_in
+      real zk(nl_max), p(nl_max), theta(nl_max), rho(nl_max), &
+           u(nl_max), v(nl_max), qv(nl_max), p_dry(nl_max), t(nl_max)
+
+      integer n
+      parameter(n=1000)
+      logical debug
+      parameter( debug = .true.)
+      character*256 message
+
+! input sounding data
+
+      real p_surf, th_surf, qv_surf
+      real pi_surf, pi(n)
+      real h_input(n), th_input(n), qv_input(n), u_input(n), v_input(n)
+
+! diagnostics
+
+      real rho_surf, p_input(n), rho_input(n)
+      real pm_input(n)  !  this are for full moist sounding
+
+! local data
+
+      real r, g,cp
+      parameter (r = rdgas)
+      parameter (g = grav)
+      parameter (cp = cp_air)
+      parameter p1000mb = 100000.0, cvpm = -718./cp_air
+      parameter rvovrd = rvgas/rdgas
+      integer k, it, nl, nl_file, istat
+      real qvf, qvf1, dz
+      character*256 line
+
+!  first, read the sounding
+   print*,"OPEN SOUNDING FILE: ", trim('input_sounding')
+   open(14, file=trim('input_sounding'), form='formatted', iostat=istat)
+   if (istat /= 0) then
+     call mpp_error(FATAL,"ERROR OPENING VARIABLE MAPPING FILE")
+   endif
+
+   nl = 0
+   nl_file = 0
+
+   !Loop over lines of file to count the number of levels
+   do
+     read(14, '(A)', iostat=istat) line
+     if (istat/=0) exit
+     if ( trim(line) .eq. '' ) cycle
+     nl_file = nl_file + 1
+   enddo
+   if ( nl_file == 0) call mpp_error(FATAL,"VARMAP FILE IS EMPTY.")
+   nl = nl_file -1
+
+   nl_in = nl
+   if(nl_in .gt. nl_max ) then
+     print*, ' too many levels for input arrays ',nl_in,nl_max
+     call mpp_error (FATAL, 'Too many levels for input arrays ' )
+    end if
+
+   rewind(14)
+   read(14,*,iostat=istat) p_surf, th_surf, qv_surf
+    do k = 2,nl_file
+      read(14, *, iostat=istat) h_input(nl_in-k+2), th_input(nl_in-k+2), qv_input(nl_in-k+2), u_input(nl_in-k+2), v_input(nl_in-k+2)
+     if (istat /= 0) call mpp_error(FATAL,"READING VARIABLE MAPPING FILE")
+   enddo
+   close(14)
+  
+   !  compute diagnostics,
+!  first, convert qv(g/kg) to qv(g/g)
+
+      do k=1,nl
+        qv_input(k) = 0.001*qv_input(k)
+      enddo
+
+      p_surf = 100.*p_surf  ! convert to pascals
+      qvf = 1. + rvovrd*qv_input(1)
+      rho_surf = 1./((r/p1000mb)*th_surf*qvf*((p_surf/p1000mb)**cvpm))
+      pi_surf = (p_surf/p1000mb)**(r/cp)
+
+
+
+!  integrate moist sounding hydrostatically, starting from the
+!  specified surface pressure
+!  -> first, integrate from surface to lowest level
+
+          qvf = 1. + rvovrd*qv_input(nl)
+          qvf1 = 1. + qv_input(nl)
+          rho_input(nl) = rho_surf
+          dz = h_input(nl)
+          do it=1,10
+            pm_input(nl) = p_surf &
+                    - 0.5*dz*(rho_surf+rho_input(nl))*g*qvf1
+            rho_input(nl) = 1./((r/p1000mb)*th_input(nl)*qvf*((pm_input(nl)/p1000mb)**cvpm))
+          enddo
+          do k=nl-1,1,-1
+            rho_input(k) = rho_input(k+1)
+            dz = h_input(k)-h_input(k+1)
+            qvf1 = 0.5*(2.+(qv_input(k+1)+qv_input(k)))
+            qvf = 1. + rvovrd*qv_input(k)   ! qv is in g/kg here
+
+            do it=1,10
+              pm_input(k) = pm_input(k+1) &
+                      - 0.5*dz*(rho_input(k)+rho_input(k+1))*g*qvf1
+              IF(pm_input(k) .LE. 0. )THEN
+                print*, "Integrated pressure has gone negative - toocold for chosen height"
+                WRITE(message,*)'k,pm_input(k),h_input(k),th_input(k) =',k,pm_input(k),h_input(k),th_input(k)
+                CALL mpp_error (FATAL, message )
+              ENDIF
+              rho_input(k) =1./((r/p1000mb)*th_input(k)*qvf*((pm_input(k)/p1000mb)**cvpm))
+            enddo
+          enddo
+
+       do k=1,nl
+
+          zk(k) = h_input(k)
+          p(k) = pm_input(k)
+          t(k) = th_input(k) * (pm_input(k) / p1000mb)**(rdgas/cp_air)
+          u(k) = u_input(k)
+          v(k) = v_input(k)
+          if(is_master()) print*, zk(k)
+          qv(k) = qv_input(k)
+
+        enddo
+  end subroutine get_sounding
+ 
+ real function interp_log( v_in, p_in, p_out, nzmax,nz_in  )
+ implicit none
+ integer, intent(in) ::  nz_in, nzmax
+ real, intent(in) ::   v_in(nzmax), p_in(nzmax)
+ real, intent(in) ::   p_out
+
+ integer kp, k, im, ip, nz_out
+ logical interp, increasing_z
+ real    pres, w1, w2
+ logical debug
+ parameter ( debug = .false. )
+
+ pres = p_out
+ if (is_master()) print*,"p_in(nz_in), p_in(1) = " , p_in(nz_in), p_in(1)
+    IF (pres > p_in(nz_in)) then
+      w2 = (log(p_in(nz_in))-log(pres))/(log(p_in(nz_in))-log(p_in(nz_in-1)))
+      w1 = 1.-w2
+      interp_log = v_in(nz_in)**w1 * v_in(nz_in-1)**w2
+    ELSE IF (pres < p_in(1)) then
+      w2 = (log(p_in(2))-log(pres))/(log(p_in(2))-log(p_in(1)))
+      w1 = 1.-w2
+      interp_log = v_in(2)**w1 * v_in(1)**w2
+    ELSE
+      interp = .false.
+      kp = nz_in
+      DO WHILE ( (interp .eqv. .false.) .and. (kp .ge. 2) )
+        IF(   ((p_in(kp)   .ge. pres) .and.     &
+               (p_in(kp-1) .le. pres))             )   THEN
+          w2 = (log(pres)-log(p_in(kp)))/(log(p_in(kp-1))-log(p_in(kp)))
+          w1 = 1.-w2
+          interp_log = v_in(kp)**w1 * v_in(kp-1)**w2
+          interp = .true.
+        END IF
+        kp = kp-1
+      ENDDO
+    ENDIF
+ end function interp_log
+
+ real function interp_lin( v_in, z_in, z_out, nzmax,nz_in  )
+ implicit none
+ integer, intent(in) ::  nz_in, nzmax
+ real, intent(in) ::   v_in(nzmax), z_in(nzmax)
+ real, intent(in) ::   z_out
+
+ integer kp, k, im, ip, nz_out
+ logical interp, increasing_z
+ real    height, w1, w2
+ logical debug
+ parameter ( debug = .true. )
+
+height = z_out
+IF (height < z_in(nz_in)) then
+      if(debug .and. is_master()) print*, ' point 1 in interp_lin ', height
+      w2 = (z_in(nz_in)-height)/(z_in(nz_in)-z_in(nz_in-1))
+      w1 = 1.-w2
+      interp_lin = w1*v_in(nz_in) + w2*v_in(nz_in-1)
+    ELSE IF (height > z_in(1)) then
+      if(debug .and. is_master()) print*, ' point 2 in interp_lin ', height
+      w2 = (z_in(2)-height)/(z_in(2)-z_in(1))
+      w1 = 1.-w2
+      interp_lin = w1*v_in(2) + w2*v_in(1)
+    ELSE
+      if(debug .and. is_master()) print*, ' point 3 in interp_lin ', height
+      interp = .false.
+      kp = nz_in
+      height = z_out
+      DO WHILE ( (interp .eqv. .false.) .and. (kp .ge. 2) )
+        IF(   ((z_in(kp)   .le. height) .and.     &
+               (z_in(kp-1) .ge. height))             )   THEN
+          w2 = (height-z_in(kp))/(z_in(kp-1)-z_in(kp))
+          w1 = 1.-w2
+          interp_lin = w1*v_in(kp) + w2*v_in(kp-1)
+          interp = .true.
+        END IF
+        kp = kp-1
+      ENDDO
+    ENDIF
+ end function interp_lin
 end module test_cases_mod
