83a04adab4dc52166d9babac39fd4435458641d4
laurenchilutti <60401591+laurenchilutti@users.noreply.github.com>
2023-04-20 15:43:11 -0400

    Release FV3 solver updates (#261)
    * Bringing over internal updates to nh_core and nh_utils which includes a revision to the semi-implicit solver to linearize vertical sound wave propagation about the hydrostatic state.
    
    * resolve inconsistent use of single and double precision in rayleigh damping.  Address spacing change.
    
    * rename tau_w
    
    * Modify the rff calculation to be fully double precision.  Fixed a mistake in the previous cherry-pick commit
    
    * fully implement the name change of tau_w to fast_tau_w_sec

--- model/fv_control.F90
+++ model/fv_control.F90
@@ -232,6 +232,7 @@ module fv_control_mod
      real    , pointer :: ke_bg
      real    , pointer :: consv_te
      real    , pointer :: tau
+     real    , pointer :: fast_tau_w_sec
      real    , pointer :: rf_cutoff
      real    , pointer :: te_err
      real    , pointer :: tw_err
@@ -763,6 +764,7 @@ module fv_control_mod
        ke_bg                         => Atm%flagstruct%ke_bg
        consv_te                      => Atm%flagstruct%consv_te
        tau                           => Atm%flagstruct%tau
+       fast_tau_w_sec                => Atm%flagstruct%fast_tau_w_sec
        rf_cutoff                     => Atm%flagstruct%rf_cutoff
        te_err                        => Atm%flagstruct%te_err
        tw_err                        => Atm%flagstruct%tw_err
@@ -930,7 +932,7 @@ module fv_control_mod
             dry_mass, grid_type, do_Held_Suarez, &
             consv_te, fill, filter_phys, fill_dp, fill_wz, fill_gfs, consv_am, RF_fast, &
             range_warn, dwind_2d, inline_q, z_tracer, reproduce_sum, adiabatic, do_vort_damp, no_dycore,   &
-            tau, tau_h2o, rf_cutoff, te_err, tw_err, nf_omega, hydrostatic, fv_sg_adj, sg_cutoff, breed_vortex_inline,  &
+            tau, fast_tau_w_sec, tau_h2o, rf_cutoff, te_err, tw_err, nf_omega, hydrostatic, fv_sg_adj, sg_cutoff, breed_vortex_inline,  &
             na_init, nudge_dz, hybrid_z, Make_NH, n_zs_filter, nord_zs_filter, full_zs_filter, reset_eta,         &
             pnats, dnats, dnrts, a2b_ord, remap_t, p_ref, d2_bg_k1, d2_bg_k2,  &
             c2l_ord, dx_const, dy_const, umax, deglat,      &
