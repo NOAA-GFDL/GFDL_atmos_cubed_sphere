c920718e07f11a48b6cadb15e5e4e90de442f65f
Lauren Chilutti <Lauren.Chilutti@noaa.gov>
2023-10-12 13:24:38 -0400

    sst fix

--- tools/external_sst.F90
+++ tools/external_sst.F90
@@ -21,17 +21,8 @@
 
 module external_sst_mod
 
-#ifdef NO_GFDL_SHARED
-!----------------- Public Data -----------------------------------
-integer :: i_sst = -1
-integer :: j_sst = -1
-logical :: forecast_mode = .false.
-logical :: use_ncep_sst  = .false.
 real, allocatable, dimension(:,:) ::  sst_ncep, sst_anom
-#else
-use amip_interp_mod, only: i_sst, j_sst, sst_ncep, sst_anom, &
-                           forecast_mode, use_ncep_sst
-#endif
+use amip_interp_mod, only: i_sst, j_sst, forecast_mode, use_ncep_sst
 
 public i_sst, j_sst, sst_ncep, sst_anom, forecast_mode, use_ncep_sst
 
