ffa851a4399290eae4a2aeeacfe34f6b5387b761
Spencer Clark <spencerkclark@gmail.com>
2024-01-24 19:48:00 +0000

    Guard behind GFS_PHYS; rename to delp_total

--- tools/fv_diagnostics.h
+++ tools/fv_diagnostics.h
@@ -105,5 +105,7 @@
      integer :: id_uw, id_vw
      integer :: id_lagrangian_tendency_of_hydrostatic_pressure
      integer :: id_t_dt_nudge, id_ps_dt_nudge, id_delp_dt_nudge, id_u_dt_nudge, id_v_dt_nudge
-     integer :: id_delp_dycore
+#ifdef GFS_PHYS
+     integer :: id_delp_total
+#endif
 #endif _FV_DIAG__
