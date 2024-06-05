1008e83a494d9700c9c2b4e8bd50ef196b60ac1a
Spencer Clark <Spencer.Clark@noaa.gov>
2024-06-05 17:59:56 -0400

    Implement t_dt_diabatic and qv_dt_diabatic diagnostics

--- tools/fv_diagnostics.h
+++ tools/fv_diagnostics.h
@@ -95,6 +95,7 @@
      integer :: id_qr_dt_phys, id_qg_dt_phys, id_qs_dt_phys
      integer :: id_liq_wat_dt_phys, id_ice_wat_dt_phys
      integer :: id_intqv, id_intql, id_intqi, id_intqr, id_intqs, id_intqg
+     integer :: id_t_dt_diabatic, id_qv_dt_diabatic
 
 ! ESM/CM 3-D diagostics
      integer :: id_uq, id_vq, id_wq, id_iuq, id_ivq, id_iwq,   & ! moisture flux & vertical integral
