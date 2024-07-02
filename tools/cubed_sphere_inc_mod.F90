ff741fd7afabf9b9e9d0079224e0c3de6853864b
DavidNew-NOAA <david.new@noaa.gov>
2024-07-02 09:28:23 -0500

    Initial commit

--- tools/cubed_sphere_inc_mod.F90
+++ tools/cubed_sphere_inc_mod.F90
@@ -3,8 +3,8 @@ module cubed_sphere_inc_mod
   use tracer_manager_mod, only: get_tracer_index, get_number_tracers, get_tracer_names
   use field_manager_mod,  only: MODEL_ATMOS
   use fv_arrays_mod,      only: fv_atmos_type
-  use sim_nc_mod,         only: open_ncfile, get_var5_r4, check_var_exists, close_ncfile
-
+  use fms2_io_mod,        only: open_file, close_file, read_data, variable_exists, FmsNetcdfFile_t
+  
   implicit none
   type increment_data_type
     real, allocatable :: ua_inc(:,:,:)
@@ -21,47 +21,42 @@ module cubed_sphere_inc_mod
 
 !----------------------------------------------------------------------------------------
 
-    subroutine read_cubed_sphere_inc(filename, increment_data, Atm)
-      character(*), intent(in)                 :: filename
+    subroutine read_cubed_sphere_inc(fname_prefix, increment_data, Atm)
+      character(*), intent(in)                 :: fname_prefix
       type(increment_data_type), intent(inout) :: increment_data
       type(fv_atmos_type), intent(in)          :: Atm
 
-      integer           :: isc, iec, jsc, jec, npz, itracer, ntracers, itile
-      character(len=64) :: tracer_name
-      integer           :: ncid, ncerr
+      type(FmsNetcdfFile_t) :: fileobj
+      integer               :: itracer, ntracers, itile
+      character(len=64)     :: tracer_name
+      character(len=1)      :: itile_str 
       
       ! Get various dimensions
       call get_number_tracers(MODEL_ATMOS, num_tracers=ntracers)
-      isc = Atm%bd%isc
-      iec = Atm%bd%iec
-      jsc = Atm%bd%jsc
-      jec = Atm%bd%jec
-      npz = Atm%npz
       itile = Atm%tile_of_mosaic
+      write(itile_str, '(I0)') itile
 
-      ! Open increment file
-      call open_ncfile( trim(filename), ncid )
-
-      ! Read increments
-      call get_var5_r4( ncid, 'u_inc', isc,iec, jsc,jec, 1,npz, itile, 1, increment_data%ua_inc )
-      call get_var5_r4( ncid, 'v_inc', isc,iec, jsc,jec, 1,npz, itile, 1, increment_data%va_inc )
-      call get_var5_r4( ncid, 'T_inc', isc,iec, jsc,jec, 1,npz, itile, 1, increment_data%temp_inc )
-      call get_var5_r4( ncid, 'delp_inc', isc,iec, jsc,jec, 1,npz, itile, 1, increment_data%delp_inc )
-      if ( .not. Atm%flagstruct%hydrostatic ) then
-         call get_var5_r4( ncid, 'delz_inc', isc,iec, jsc,jec, 1,npz, itile, 1, increment_data%delz_inc )
-      end if
-
-      ! Read tracer increments
-      do itracer = 1,ntracers
-         call get_tracer_names(MODEL_ATMOS, itracer, tracer_name)
-         call check_var_exists(ncid, trim(tracer_name)//'_inc', ncerr)
-         if ( ncerr == 0 ) then
-            call get_var5_r4( ncid, trim(tracer_name)//'_inc', isc,iec, jsc,jec, 1,npz, itile, 1, increment_data%tracer_inc(:,:,:,itracer) )
+      ! Open file
+      if (open_file(fileobj, trim(fname_prefix) // '.tile' // itile_str // '.nc', "read", Atm%domain)) then
+         ! Read increments
+         call read_data(fileobj, 'u_inc', increment_data%ua_inc)
+         call read_data(fileobj, 'v_inc', increment_data%va_inc)
+         call read_data(fileobj, 'T_inc', increment_data%T_inc)
+         call read_data(fileobj, 'delp_inc', increment_data%delp_inc)
+         if ( .not. Atm%flagstruct%hydrostatic ) then
+            call read_data(fileobj, 'delz_inc', increment_data%delz_inc)
          end if
-      end do
+
+         ! Read tracer increments
+         do itracer = 1,ntracers
+            call get_tracer_names(MODEL_ATMOS, itracer, tracer_name)
+            if ( variable_exists(fileobj, trim(tracer_name)//'_inc') ) then
+               call read_data(fileobj, trim(tracer_name)//'_inc', increment_data%tracer_inc(:,:,:,itracer))
+            end if
+         end do
          
-      ! Close increment file
-      call close_ncfile(ncid)
+         call close_file(fileobj)
+      end if
     
   end subroutine read_cubed_sphere_inc
 
