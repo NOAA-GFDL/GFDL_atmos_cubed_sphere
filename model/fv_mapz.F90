d89f8d16d7607356859482d2d46fc0f99b4c863a
dpsarmie <Daniel.Sarmiento@noaa.gov>
2025-08-14 14:11:17 -0500

    Fix bug in nested logic

--- model/fv_mapz.F90
+++ model/fv_mapz.F90
@@ -3409,8 +3409,8 @@ endif        ! end last_step check
 #endif
                         dpsum = dpsum + delp
                      endif
-                     q2(i,k) = qsum / dpsum
                   endif
+                  q2(i,k) = qsum / dpsum
                endif
             endif
          enddo
