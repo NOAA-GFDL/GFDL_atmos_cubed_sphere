e616f39a07adc860b16ead24b8f88371c7793507
Denise Worthen <denise.worthen@noaa.gov>
2026-05-05 12:46:19 -0400

    add cpl_imp_mrg/dbg to atmos_model nml

--- driver/UFS/atmosphere.F90
+++ driver/UFS/atmosphere.F90
@@ -332,8 +332,10 @@ contains
    logical :: sync         = .false.
    logical :: ignore_rst_cksum = .false.
    real    :: avg_max_length = 3600.
+   logical :: cpl_imp_mrg = .false.
+   logical :: cpl_imp_dbg = .false.
    namelist /atmos_model_nml/ blocksize, chksum_debug, dycore_only, debug, sync, ccpp_suite, avg_max_length, &
-                              ignore_rst_cksum
+                              ignore_rst_cksum, cpl_imp_mrg, cpl_imp_dbg
    ! *DH 20210326
 
    !For regional
